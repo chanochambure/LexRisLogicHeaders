74f2adb44a00cc7d18ac98cb78b7e604b6bed35d
chanochambure <christian.benavides@ucsp.edu.pe>
2015-11-05 09:42:34 -0500

    Correct Name for ENet Headers

---
+++ ENet/LL_ENet.h
@@ -0,0 +1,18 @@
+#ifndef LL_ENET_H_INCLUDED
+#define LL_ENET_H_INCLUDED
+
+//Enet
+#include <enet/enet.h>
+#include <LexRisLogic/LL_Convert.h>
+
+//Functions
+
+bool install_enet(){return !(enet_initialize());}
+
+void uninstall_enet(){enet_deinitialize();}
+
+//Library
+#include "LL_Server.h"
+#include "LL_Client.h"
+
+#endif // LL_ENET_H_INCLUDED
