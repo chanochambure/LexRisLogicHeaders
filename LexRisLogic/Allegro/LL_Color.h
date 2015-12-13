b104c4233430e8670fdd0837f49486b7e43bf057
chanochambure <christian.benavides@ucsp.edu.pe>
2015-12-13 01:44:54 -0500

    LL_Color.h
    Color Error operator = ALLEGRO COLOR

--- LexRisLogic/Allegro/LL_Color.h
+++ LexRisLogic/Allegro/LL_Color.h
@@ -3,14 +3,14 @@
 
 struct Color
 {
-    char Red=0;
-    char Green=0;
-    char Blue=0;
-    char Alpha=255;
-    Color(char r=0,char g=0,char b=0,char a=255){Red=r;Green=g;Blue=b,Alpha=a;}
+    unsigned char Red=0;
+    unsigned char Green=0;
+    unsigned char Blue=0;
+    unsigned char Alpha=255;
+    Color(unsigned char r=0,unsigned char g=0,unsigned char b=0,unsigned char a=255){Red=r;Green=g;Blue=b,Alpha=a;}
     operator ALLEGRO_COLOR(){return al_map_rgba(Red,Green,Blue,Alpha);}
     Color operator ! (){return Color(255-Red,255-Green,255-Blue,Alpha);}
-    Color operator = (ALLEGRO_COLOR Ot){Red=Ot.r;Green=Ot.g;Blue=Ot.b;Alpha=Ot.a;return (*this);}
+    Color operator = (ALLEGRO_COLOR Ot){Red=(Ot.r*255);Green=(Ot.g*255);Blue=(Ot.b*255);Alpha=(Ot.a*255);return (*this);}
 };
 
 #endif // LL_COLOR_H_INCLUDED
