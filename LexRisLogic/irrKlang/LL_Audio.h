35f23b7aed4ce274daee1975e73d44da0cf38f1a
chanochambure <christian.benavides@ucsp.edu.pe>
2016-03-11 00:21:45 -0500

    LL_irrKlang::LL_Audio
    Modified play function to replay when the audio will finish

--- LexRisLogic/irrKlang/LL_Audio.h
+++ LexRisLogic/irrKlang/LL_Audio.h
@@ -23,28 +23,18 @@ namespace LL_irrKlang
             void set_volume(float vol){if(sound)sound->setVolume(vol*default_engine->get_volume());vl=vol;}
             float get_volume(){return vl;}
             void set_mode(bool looped){loop=looped;if(sound)sound->setIsLooped(loop);}
-            bool load();
+            bool load(){destroy();if(!default_engine)return 0;sound=((irrklang::ISoundEngine*)(*default_engine))->play2D(audio_path.c_str(),loop,true);_ps=0;set_volume(vl);return sound;}
             unsigned int get_size(){if(sound)return sound->getPlayLength();return 0;}
             void set_position(unsigned int pos){if(sound)sound->setPlayPosition(_ps=pos);}
             unsigned int get_position(){if(sound)return sound->getPlayPosition();return 0;}
-            bool is_playing(){return (sound and !(sound->getIsPaused() or sound->isFinished()));}
+            bool is_playing(){if(sound and sound->isFinished()){_ps=0;return 0;}return (sound and !sound->getIsPaused());}
             void stop(){if(sound){sound->stop();_ps=0;}}
             void pause(){if(is_playing()){_ps=get_position();sound->setIsPaused(true);}}
-            void play(){if(sound and !is_playing()){set_position(_ps);sound->setIsPaused(false);}}
+            void play(){if(sound){if(!sound->isFinished()){if(sound->getIsPaused()){set_position(_ps);sound->setIsPaused(false);}}else{float t_speed=get_speed();float t_pan=get_pan();load();sound->setIsPaused(false);set_speed(t_speed);set_pan(t_pan);}}}
             bool destroy(){stop();if(!sound)return 0;sound->drop();sound=nullptr;return 1;}
             operator irrklang::ISound* (){return sound;}
             ~LL_Audio(){destroy();}
     };
-
-    bool LL_Audio::load()
-    {
-        destroy();
-        if(!default_engine)
-            return 0;
-        sound=((irrklang::ISoundEngine*)(*default_engine))->play2D(audio_path.c_str(),loop,true);
-        set_volume(vl);
-        return sound;
-    }
 }
 
 #endif // LL_IRRKLANG_AUDIO_H_INCLUDED
