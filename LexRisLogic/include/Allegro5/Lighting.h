fa20d295b6be46f8b422a71fa9bef3901bddb436
chanochambure <christian.benavides@ucsp.edu.pe>
2016-11-09 03:58:13 -0500

    Allegro - Sustitution of File (Illumination-Lighting)
    Iluminación usando canal alfa y clases templates Removida
    
    Se creo un Nuevo tipo de iluminación usando operaciones de blending
    (Fusión de colores en la operación de dibujo), ya no es del tipo
    especial, forma parte de la librería de allegro.

---
+++ LexRisLogic/include/Allegro5/Lighting.h
@@ -0,0 +1,143 @@
+/* Lighting.h -- Lighting Allegro 5 Header - LexRis Logic Headers
+
+    Copyright (c) 2016 LexRisLogic
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+    documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+    rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so.
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+    the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+    THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+#ifndef INCLUDED_LL_AL5_LIGHTING_H
+#define INCLUDED_LL_AL5_LIGHTING_H
+
+#include "Allegro5.h"
+
+namespace LL_AL5
+{
+    class Lighting
+    {
+        private:
+            int _V_blend_mode;
+            int _V_blend_source_value;
+            int _V_blend_destination_value;
+            ALLEGRO_BITMAP* _V_lighting_buffer=nullptr;
+            Type_pos _V_pos_x=0.0;
+            Type_pos _V_pos_y=0.0;
+            float _V_size_x=0.0;
+            float _V_size_y=0.0;
+        public:
+            Lighting()
+            {
+                al_get_blender(&_V_blend_mode,&_V_blend_source_value,&_V_blend_destination_value);
+            }
+            void set_pos(Type_pos new_pos_x,Type_pos new_pos_y)
+            {
+                _V_pos_x=new_pos_x;
+                _V_pos_y=new_pos_y;
+            }
+            void set_pos_x(Type_pos new_pos_x)
+            {
+                _V_pos_x=new_pos_x;
+            }
+            Type_pos get_pos_x()
+            {
+                return _V_pos_x;
+            }
+            void set_pos_y(Type_pos new_pos_y)
+            {
+                _V_pos_y=new_pos_y;
+            }
+            Type_pos get_pos_y()
+            {
+                return _V_pos_y;
+            }
+            float get_size_x()
+            {
+                return _V_size_x;
+            }
+            float get_size_y()
+            {
+                return _V_size_y;
+            }
+            bool create(int size_x,int size_y)
+            {
+                destroy();
+                _V_lighting_buffer=al_create_bitmap(size_x,size_y);
+                if(_V_lighting_buffer)
+                {
+                    _V_size_x=size_x;
+                    _V_size_y=size_y;
+                    return true;
+                }
+                return false;
+            }
+            bool destroy()
+            {
+                if(_V_lighting_buffer)
+                {
+                    al_destroy_bitmap(_V_lighting_buffer);
+                    _V_lighting_buffer=nullptr;
+                    return true;
+                }
+                return false;
+            }
+            void set_target()
+            {
+                if(_V_lighting_buffer)
+                    al_set_target_bitmap(_V_lighting_buffer);
+            }
+            void clear()
+            {
+                al_set_blender(ALLEGRO_SRC_MINUS_DEST, ALLEGRO_ONE, ALLEGRO_ZERO);
+                al_clear_to_color(al_map_rgba(0,0,0,0));
+                al_set_blender(_V_blend_mode, _V_blend_source_value, _V_blend_destination_value);
+            }
+            template<typename T>
+            void add(T* object_shape,float intensity)
+            {
+                al_set_blender(ALLEGRO_SRC_MINUS_DEST, ALLEGRO_ONE, ALLEGRO_ZERO);
+                ALLEGRO_COLOR data_color=object_shape->get_color();
+                object_shape->set_color(al_map_rgba_f(0,0,0,intensity));
+                object_shape->draw_in_another_target();
+                al_set_blender(_V_blend_mode, _V_blend_source_value, _V_blend_destination_value);
+                object_shape->set_color(data_color);
+            }
+            float get_pixel_intensity(Type_pos pos_x,Type_pos pos_y)
+            {
+                return al_get_pixel(_V_lighting_buffer,pos_x,pos_y).a;
+            }
+            void draw()
+            {
+                al_set_blender(ALLEGRO_ADD, ALLEGRO_ZERO, ALLEGRO_ALPHA);
+                al_draw_scaled_bitmap(_V_lighting_buffer,0,0,_V_size_x,_V_size_y,_V_pos_x,_V_pos_y,
+                                      _V_size_x*bitmap_scale_x,_V_size_y*bitmap_scale_y,0);
+                al_set_blender(_V_blend_mode, _V_blend_source_value, _V_blend_destination_value);
+            }
+            void draw_in_another_target()
+            {
+                al_set_blender(ALLEGRO_ADD, ALLEGRO_ZERO, ALLEGRO_ALPHA);
+                al_draw_bitmap(_V_lighting_buffer,_V_pos_x,_V_pos_y,0);
+                al_set_blender(_V_blend_mode, _V_blend_source_value, _V_blend_destination_value);
+            }
+            operator ALLEGRO_BITMAP* ()
+            {
+                return _V_lighting_buffer;
+            }
+            ~Lighting()
+            {
+                destroy();
+            }
+    };
+}
+
+#endif // INCLUDED_LL_AL5_LIGHTING_H
